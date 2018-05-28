ac88455ed8ecf30475134b128c780746d1400c8a
guimigli <32096477+guimigli@users.noreply.github.com>
2018-05-28 18:26:20 -0300

    Update Jedi.java
    Atualização de habilidades

--- src/lista07/Jedi.java
+++ src/lista07/Jedi.java
@@ -4,7 +4,13 @@ package lista07;
 public class Jedi extends UsuarioDaForca{
 	private String corCapuz;
 	
-
+	Aprendiz(String nome, double vida) {
+		this.addHabilidade(new Habilidade("sabre","Ataque simples",50,2));
+		this.addHabilidade(new Habilidade("forca","Empurrão fraco",25,3));
+		this.setNome(nome);
+		this.setVida(vida);
+	}
+	
 	public String getCorCapuz() {
 		return corCapuz;
 	}
